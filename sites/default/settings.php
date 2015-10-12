35f1796f71ff371999c7835a1df1f52def19a0e2
Alex Costa & charles moss <student@epicodus.com>
2015-10-12 12:21:19 -0700

    add logo

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'fansite',
+      'username' => 'fansite_admin',
+      'password' => 'treehouse',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '6y7QA6y2AmMB4O1-cwoJOXYQh_sfaQFbARgq33Itz-Q';
 
 /**
  * Base URL (optional).
